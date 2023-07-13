74ed145d167e746199123ff57c4d82a17449ece1
polirritmico <ebray187@gmail.com>
2023-07-13 02:53:24 -0400

    Move base colors and formats to the palette folder

--- lua/monokai.lua
+++ lua/monokai.lua
@@ -5,52 +5,12 @@
 local vim = vim
 local M = {}
 
--- Set color palette and styles
-M.theme_name = "monokai"
-
-M.color = {
-    black = "#000000",
-    blood_red = "#5F0000",
-    bright_blue = "#0087FF",
-    charcoal = "#262626",
-    danger = "#FF005F",
-    dark_green = "#005F00",
-    dark_grey = "#5F5F5F",
-    dark_red = "#870000",
-    darker_grey = "#444444",
-    grey = "#8A8A8A",
-    light_blue = "#62D8F1",
-    light_charcoal = "#2B2B2B",
-    light_green = "#A4E400",
-    light_grey = "#BCBCBC",
-    magenta = "#FC1A70",
-    off_white = "#CCCCCC",
-    olive = "#5F8700",
-    orange = "#FF9700",
-    purple = "#AF87FF",
-    purple_slate = "#5F5F87",
-    white = "#FFFFFF",
-    yellow = "#FFFF87",
-
-    none = "NONE",
-}
-
-M.format = {
-    bold = "bold",
-    italic = "italic",
-    underline = "underline",
-    bold_underline = "bold,underline",
-    none = "NONE",
-}
-
--- Set background color
-M.background = M.color.charcoal
-
--- Set italics
-M.enable_italics = true
+local monokai_theme = require("palette.base-style")
+local c = monokai_theme.colors
+local f = monokai_theme.formats
 
 -- Helper functions
-local function set_style(group, style)
+local apply_style = function(group, style)
     local format = style.format and "gui = " .. style.format or "gui = NONE"
     local fg = style.fg and "guifg = " .. style.fg or "guifg = NONE"
     local bg = style.bg and "guibg = " .. style.bg or "guibg = NONE"
@@ -59,10 +19,29 @@ local function set_style(group, style)
     )
 end
 
--- Set themes
-local c = M.color
-local f = M.format
-M.theme_base = {
+local set_theme = function(theme)
+    for highlight_group, style in pairs(theme) do
+        apply_style(highlight_group, style)
+    end
+end
+
+local set_background = function(transparent, theme)
+    if transparent then
+        theme.Normal.bg = c.none
+    end
+end
+
+local remove_italics = function(theme)
+    for _, style in pairs(theme) do
+        if style.format and style.format == "italic" then
+            style.format = nil
+        end
+    end
+end
+
+
+-- Theme settings
+local theme_base = {
     Boolean = { fg = c.purple, bg = c.none, format = f.none },
     Character = { fg = c.purple, bg = c.none, format = f.none },
     ColorColumn = { fg = c.none, bg = c.light_charcoal, format = f.none },
@@ -84,12 +63,6 @@ M.theme_base = {
     DiffDelete = { fg = c.dark_red, bg = c.blood_red, format = f.none },
     DiffText = { fg = c.white, bg = c.bright_blue, format = f.bold },
     Directory = { fg = c.purple, bg = c.none, format = f.none },
-    diffAdded = { fg = c.light_green, bg = c.none, format = f.none },
-    diffFile = { fg = c.white, bg = c.none, format = f.bold },
-    diffIndexLine = { fg = c.purple, bg = c.none, format = f.bold },
-    diffLine = { fg = c.purple, bg = c.none, format = f.bold },
-    diffRemoved = { fg = c.magenta, bg = c.none, format = f.none },
-    diffSubname = { fg = c.yellow, bg = c.none, format = f.none },
     EndOfBuffer = { fg = c.darker_grey, bg = c.none, format = f.none },
     Error = { fg = c.white, bg = c.danger, format = f.none },
     ErrorMsg = { fg = c.white, bg = c.danger, format = f.none },
@@ -98,10 +71,6 @@ M.theme_base = {
     FoldColumn = { fg = c.grey, bg = c.none, format = f.none },
     Folded = { fg = c.grey, bg = c.none, format = f.none },
     Function = { fg = c.light_green, bg = c.none, format = f.none },
-    gitcommitHeader = { fg = c.light_blue, bg = c.none, format = f.none },
-    gitcommitOverflow = { fg = c.magenta, bg = c.none, format = f.none },
-    gitcommitSelectedFile = { fg = c.orange, bg = c.none, format = f.none },
-    gitcommitSummary = { fg = c.white, bg = c.none, format = f.none },
     Identifier = { fg = c.light_blue, bg = c.none, format = f.none },
     IncSearch = { fg = c.black, bg = c.purple, format = f.bold_underline },
     Include = { fg = c.magenta, bg = c.none, format = f.none },
@@ -109,13 +78,12 @@ M.theme_base = {
     Label = { fg = c.yellow, bg = c.none, format = f.none },
     LineNr = { fg = c.dark_grey, bg = c.none, format = f.none },
     LineNrBe = { fg = c.light_green, format = f.none },
-    lCursor = { fg = c.charcoal, bg = c.light_blue, format = f.none },
     Macro = { fg = c.magenta, bg = c.none, format = f.none },
     MatchParen = { fg = c.magenta, bg = c.none, format = f.bold_underline },
     ModeMsg = { fg = c.white, bg = c.none, format = f.bold },
     MoreMsg = { fg = c.light_green, bg = c.none, format = f.none },
     NonText = { fg = c.darker_grey, bg = c.none, format = f.none },
-    Normal = { fg = c.white, bg = c.none },
+    Normal = { fg = c.white, bg = c.charcoal },
     Number = { fg = c.purple, bg = c.none, format = f.none },
     Operator = { fg = c.magenta, bg = c.none, format = f.none },
     Pmenu = { fg = c.light_blue, bg = c.darker_grey, format = f.none },
@@ -157,24 +125,49 @@ M.theme_base = {
     WarningMsg = { fg = c.white, bg = c.danger, format = f.none },
     Whitespace = { fg = c.dark_grey, bg = c.none, format = f.none },
     WinSeparator = { fg = c.darker_grey, bg = c.none, format = f.none },
+    diffAdded = { fg = c.light_green, bg = c.none, format = f.none },
+    diffFile = { fg = c.white, bg = c.none, format = f.bold },
+    diffIndexLine = { fg = c.purple, bg = c.none, format = f.bold },
+    diffLine = { fg = c.purple, bg = c.none, format = f.bold },
+    diffRemoved = { fg = c.magenta, bg = c.none, format = f.none },
+    diffSubname = { fg = c.yellow, bg = c.none, format = f.none },
+    gitcommitHeader = { fg = c.light_blue, bg = c.none, format = f.none },
+    gitcommitOverflow = { fg = c.magenta, bg = c.none, format = f.none },
+    gitcommitSelectedFile = { fg = c.orange, bg = c.none, format = f.none },
+    gitcommitSummary = { fg = c.white, bg = c.none, format = f.none },
+    lCursor = { fg = c.charcoal, bg = c.light_blue, format = f.none },
+}
+-- Languages
+local languages = {}
+
+-- Default settings
+local defaults = {
+    theme = theme_base,
+    disable_italics = false,
+    transparent_background = true,
 }
 
-local set_theme = function(theme)
-    for highlight_group, style in pairs(theme) do
-        set_style(highlight_group, style)
-    end
-end
-
 -- Setup
-M.setup = function()
+M.setup = function(usr_config)
+    -- Clean nvim defaults and set needed options
     vim.cmd("hi clear")
     if vim.fn.exists("syntax_on") then
         vim.cmd("syntax reset")
     end
     vim.o.background = "dark"
     vim.o.termguicolors = true
-    vim.g.colors_name = M.theme_name
-    set_theme(M.theme_base)
+
+    -- Parse usr configs
+    usr_config = usr_config and usr_config or {}
+    local config = vim.tbl_deep_extend("keep", defaults, usr_config)
+
+    -- Set the theme
+    vim.g.colors_name = config.theme.name
+    set_background(config.transparent_background, config.theme)
+    if config.disable_italics then
+        config.theme = remove_italics(config.theme)
+    end
+    set_theme(config.theme)
 end
 
 return M
