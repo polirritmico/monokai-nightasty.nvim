52b6f20f474f9499dc31ffa78fb31bb2a2fefab9
polirritmico <ebray187@gmail.com>
2023-07-28 20:36:57 -0400

    Changed colors of Lualine and minor format changes

--- lua/lualine/themes/monokai.lua
+++ lua/lualine/themes/monokai.lua
@@ -4,40 +4,40 @@ local config = require("monokai.config").options
 local monokai = {}
 
 monokai.normal = {
-  a = { bg = colors.green_light, fg = colors.black },
-  b = { bg = colors.charcoal_light, fg = colors.green_light },
-  c = { bg = colors.bg_statusline, fg = colors.fg_sidebar },
+  a = { fg = colors.black, bg = colors.green_light },
+  b = { fg = colors.green_light, bg = colors.charcoal_light },
+  c = { fg = colors.fg_sidebar, bg = colors.bg_statusline },
 }
 
 monokai.insert = {
-  a = { bg = colors.yellow_light, fg = colors.black },
-  b = { bg = colors.charcoal_light, fg = colors.yellow_light },
+  a = { fg = colors.black, bg = colors.yellow_light },
+  b = { fg = colors.yellow_light, bg = colors.charcoal_light },
 }
 
 monokai.command = {
-  a = { bg = colors.yellow_light, fg = colors.black },
-  b = { bg = colors.charcoal_light, fg = colors.yellow_light },
+  a = { fg = colors.green_light, bg = colors.black },
+  b = { fg = colors.green_light, bg = colors.charcoal_light },
 }
 
 monokai.visual = {
-  a = { bg = colors.purple, fg = colors.black },
-  b = { bg = colors.charcoal_light, fg = colors.fg },
+  a = { fg = colors.black, bg = colors.purple },
+  b = { fg = colors.fg, bg = colors.charcoal_light },
 }
 
 monokai.replace = {
-  a = { bg = colors.red, fg = colors.black },
-  b = { bg = colors.charcoal_light, fg = colors.red },
+  a = { fg = colors.black, bg = colors.red },
+  b = { fg = colors.red, bg = colors.charcoal_light },
 }
 
 monokai.terminal = {
-  a = {bg = colors.green_alt, fg = colors.black },
-  b = {bg = colors.charcoal_light, fg=colors.green_alt },
+  a = { fg = colors.green_light, bg = colors.black },
+  b = { fg = colors.green_light, bg = colors.charcoal_light },
 }
 
 monokai.inactive = {
-  a = { bg = colors.bg_dark, fg = colors.blue_light },
-  b = { bg = colors.bg_dark, fg = colors.fg_gutter, gui = "bold" },
-  c = { bg = colors.bg_dark, fg = colors.fg_gutter },
+  a = { fg = colors.blue_light, bg = colors.bg_dark },
+  b = { fg = colors.fg_gutter, bg = colors.bg_dark, gui = "bold" },
+  c = { fg = colors.grey, bg = colors.bg_dark },
 }
 
 if config.lualine_bold then
