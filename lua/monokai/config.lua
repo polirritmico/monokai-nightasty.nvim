bda6b86bdeff845294f9f2cf125546337904c4fb
polirritmico <ebray187@gmail.com>
2023-08-21 05:07:22 -0400

    Change default dark background

--- lua/monokai/config.lua
+++ lua/monokai/config.lua
@@ -5,7 +5,7 @@ local M = {}
 ---@field on_highlights fun(highlights: Highlights, colors: ColorScheme)
 local defaults = {
     dark_style = {
-        transparent = true,
+        transparent = false,
         darker_background = false,
     },
     light_style = {
@@ -13,23 +13,20 @@ local defaults = {
         darker_background = false,
     },
     terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+    -- TODO: change to hl_styles or highlight_styles
     styles = {
         -- Style to be applied to different syntax groups
         -- Value is any valid attr-list value for `:help nvim_set_hl`
-        -- TODO: Add an example
         comments = { italic = true },
         keywords = { italic = true },
         functions = {},
         variables = {},
         -- Background styles. Can be "dark", "transparent" or "default"
-        -- TODO: Move this to dark_style, light_style tables?
+        -- TODO: Move this to each style?
         sidebars = "dark", -- style for sidebars, see below
         floats = "dark", -- style for floating windows
     },
     sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
-
-    -- FIXME: Remove this
-    day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
 
     hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
     dim_inactive = false, -- dims inactive windows
