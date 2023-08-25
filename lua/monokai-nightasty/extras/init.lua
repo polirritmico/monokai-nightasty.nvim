1b44801520636a4d3e14412704a4b8dbed972e06
polirritmico <ebray187@gmail.com>
2023-08-25 17:37:34 -0400

    Update path

--- lua/monokai-nightasty/extras/init.lua
+++ lua/monokai-nightasty/extras/init.lua
@@ -7,7 +7,7 @@ M.extras = {tmux = {ext = "tmux", url = "https://github.com/tmux/tmux/wiki", lab
     -- xresources = {ext = "Xresources", url = "https://wiki.archlinux.org/title/X_resources", label = "Xresources"},
     -- lua = {ext = "lua", url = "https://www.lua.org", label = "Lua Table for testing"},
     -- zathura = {ext = "zathurarc", url = "https://pwmt.org/projects/zathura/", label = "Zathura"},
-    palettes = {ext = "Palettes", url = "https://github.com/polirritmico/monokai-nightasty.nvim/tree/main/extras/palettes"},
+    palettes = {ext = "lua", url = "https://github.com/polirritmico/monokai-nightasty.nvim/tree/main/extras/palettes"},
 }
 
 local function write(str, fileName)
