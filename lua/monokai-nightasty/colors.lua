3f118e90bcf845d86e067c2e033cf0e918d239f4
polirritmico <ebray187@gmail.com>
2024-01-03 16:07:07 -0300

    Change bg_sidebar color to separate sidebars from normal buffers

--- lua/monokai-nightasty/colors.lua
+++ lua/monokai-nightasty/colors.lua
@@ -130,7 +130,7 @@ function M.setup(opts)
     colors.bg_sidebar = config.options.hl_styles.sidebars == "transparent" and colors.none
         or config.options.hl_styles.sidebars == "dark" and colors.bg_dark
         or is_light and colors.bg_statusline
-        or colors.charcoal_medium
+        or colors.charcoal
     colors.bg_float = config.options.hl_styles.floats == "transparent" and colors.none
         or (config.options.hl_styles.floats == "dark" and is_light) and colors.charcoal_medium
         or config.options.hl_styles.floats == "dark" and colors.bg_dark
