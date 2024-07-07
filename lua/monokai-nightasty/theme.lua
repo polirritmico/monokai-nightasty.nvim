e1c604c2eb5d89d18202fde4d22a921a57bdf3b4
polirritmico <ebray187@gmail.com>
2024-07-07 19:32:28 -0400

    feat: Improve inlay hints readability

--- lua/monokai-nightasty/theme.lua
+++ lua/monokai-nightasty/theme.lua
@@ -178,7 +178,7 @@ function M.setup(palette)
 
     -- LspSignatureActiveParameter = { bg = util.darken(c.bg_visual, 0.4), bold = true },
     -- LspCodeLens = { fg = c.comment },
-    -- LspInlayHint = { bg = util.darken(c.blue7, 0.1), fg = c.grey_dark },
+    LspInlayHint = { fg = c.grey, italic = true },
     LspInfoBorder = { fg = c.border_highlight, bg = c.bg_float },
 
     -- checkhealt
