c831b09fd4d96658cb391b449510f2fe1286f9fb
polirritmico <ebray187@gmail.com>
2024-07-23 16:12:51 -0400

    feat: Add gitsigns highlights

---
+++ lua/monokai-nightasty/highlights/gitsigns.lua
@@ -0,0 +1,18 @@
+local M = {}
+
+M.url = "https://github.com/lewis6991/gitsigns.nvim"
+
+---@type monokai.HighlightsFn
+function M.get(c, _)
+  return {
+    GitSignsCurrentLineBlame = { fg = c.info, bg = c.bg_popup, italic = true },
+    GitSignsAdd = "diffAdded",
+    GitSignsChange = "diffChanged",
+    GitSignsDelete = "diffRemoved",
+    GitSignsAddInline = { sp = c.green_alt, undercurl = true },
+    GitSignsChangeInline = { sp = c.blue_alt, undercurl = true },
+    GitSignsDeleteInline = { fg = c.diff.delete, bg = c.magenta },
+  }
+end
+
+return M
