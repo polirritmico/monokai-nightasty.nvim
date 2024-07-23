b7e49d1ca4b17f4e41266565b3c0ab3cc6b0c6e6
polirritmico <ebray187@gmail.com>
2024-07-23 16:18:40 -0400

    feat: Add nvim-treesitter-context highlights

---
+++ lua/monokai-nightasty/highlights/treesitter-context.lua
@@ -0,0 +1,15 @@
+local M = {}
+
+M.url = "https://github.com/nvim-treesitter/nvim-treesitter-context"
+
+---@type monokai.HighlightsFn
+function M.get(c, _)
+  return {
+    TreesitterContext = { bg = c.bg_sidebar },
+    TreesitterContextBottom = { sp = c.border, underline = true },
+    TreesitterContextLineNumber = { fg = c.grey_medium, bg = c.bg_sidebar },
+    TreesitterContextLineNumberBottom = { fg = c.yellow, sp = c.border, underline = true },
+  }
+end
+
+return M
