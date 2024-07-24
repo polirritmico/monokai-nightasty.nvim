dea243019d78432870714d3d47097e5d75f67e91
polirritmico <ebray187@gmail.com>
2024-07-23 20:05:46 -0400

    feat: Add rainbow-delimiters.nvim highlights

---
+++ lua/monokai-nightasty/highlights/rainbows.lua
@@ -0,0 +1,18 @@
+local M = {}
+
+M.url = "https://github.com/HiPhish/rainbow-delimiters.nvim"
+
+---@type monokai.HighlightsFn
+function M.get(c, _)
+  return {
+    RainbowDelimiterRed = { fg = c.magenta },
+    RainbowDelimiterOrange = { fg = c.orange },
+    RainbowDelimiterYellow = { fg = c.yellow },
+    RainbowDelimiterGreen = { fg = c.green },
+    RainbowDelimiterBlue = { fg = c.blue_alt },
+    RainbowDelimiterViolet = { fg = c.purple },
+    RainbowDelimiterCyan = { fg = c.blue },
+  }
+end
+
+return M
