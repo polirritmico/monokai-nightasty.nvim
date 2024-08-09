ea64f29337f96580685936086ea161abaa5750e6
polirritmico <ebray187@gmail.com>
2024-08-09 15:33:20 -0400

    fix: Correct typo on monokai.HighlightsFn annotation type

--- lua/monokai-nightasty/types.lua
+++ lua/monokai-nightasty/types.lua
@@ -3,7 +3,7 @@
 
 ---@alias monokai.Highlights table<string, monokai.Highlight|string>
 
----@alias monokai.HighlightsFn fun(colors.Colorscheme, opts:monokai.Config):monokai.Highlights
+---@alias monokai.HighlightsFn fun(colors:ColorScheme, opts:monokai.Config):monokai.Highlights
 
 ---@class monokai.Cache
 ---@field colors ColorScheme
