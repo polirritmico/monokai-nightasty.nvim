77bacdd3d05a7d1265f5848e4973db26e1aff353
polirritmico <ebray187@gmail.com>
2024-12-16 19:52:27 -0300

    chore: Remove commented code

--- lua/monokai-nightasty/highlights/kinds.lua
+++ lua/monokai-nightasty/highlights/kinds.lua
@@ -1,13 +1,5 @@
 local M = {}
 
--- Kind plugins
--- local kind_plugins = {
---   "Aerial%sIcon",
---   "CmpItemKind%s",
---   "NavicIcons%s",
---   "NoiceCompletionItemKind%s",
--- }
-
 local kinds_links = {
   Array = "@punctuation.bracket",
   Boolean = "Boolean",
@@ -22,13 +14,11 @@ local kinds_links = {
   File = "Normal",
   Folder = "Directory",
   Function = "@function",
-  -- FIX: target hl should be defined somewhere
   Interface = "KindParameter", -- "@lsp.type.interface",
   Key = "@variable.member",
   Keyword = "@keyword",
   Method = "@function.method",
   Module = "Special",
-  -- FIX: target hl should be defined somewhere
   Namespace = "KindText",
   Null = "@constant.builtin",
   Number = "@number",
@@ -37,15 +27,12 @@ local kinds_links = {
   Package = "@module",
   Property = "@property",
   Reference = "@markup.link.url", -- @markup.link
-  -- FIX: target hl should be defined somewhere
   Snippet = "KindConceal", -- Conceal
   String = "@string",
   Struct = "Structure", -- "@lsp.type.struct",
   Text = "@markup",
-  -- FIX: target hl should be defined somewhere
   TypeParameter = "KindParameter",
   Unit = "Structure",
-  -- FIX: target hl should be defined somewhere
   Value = "KindParameter",
   Variable = "@variable",
 }
@@ -67,12 +54,6 @@ end
 
 ---@type monokai.HighlightsFn
 function M.get()
-  -- local references = {
-  --   KindConceal = { fg = c.blue_alt },
-  --   KindParameter = { fg = c.orange },
-  --   KindText = { fg = c.fg },
-  -- }
-
   return M.kinds()
 end
 
