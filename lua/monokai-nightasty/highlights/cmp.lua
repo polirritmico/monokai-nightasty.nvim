92bc4ff6f96200818d60b46cccd4e920e0700a14
polirritmico <ebray187@gmail.com>
2024-12-16 23:17:27 -0300

    feat(cmp): Improve highlight groups and clean module

--- lua/monokai-nightasty/highlights/cmp.lua
+++ lua/monokai-nightasty/highlights/cmp.lua
@@ -7,22 +7,14 @@ function M.get(c)
   local ret = {
     CmpDocumentation = { fg = c.fg, bg = c.bg_float },
     CmpDocumentationBorder = { fg = c.border_highlight, bg = c.bg_float },
-    CmpGhostText = { fg = c.grey_medium, italic = true }, -- Experimental. hl must be defined by the user
+    CmpGhostText = { fg = c.grey_medium, bg = c.none, italic = true }, -- Experimental. hl must be defined by the user
 
-    -- Left side
     CmpItemAbbr = { fg = c.fg, bg = c.none }, -- The suggestions
+    CmpItemAbbrDeprecated = { fg = c.fg_gutter, bg = c.none, strikethrough = true }, -- Deprecated suggestions
     CmpItemAbbrMatch = { fg = c.magenta, bg = c.none }, -- The current matches with the suggestions
     CmpItemAbbrMatchFuzzy = { fg = c.magenta, bg = c.none }, -- Matches ahead current
-    CmpItemAbbrDeprecated = { fg = c.fg_gutter, strikethrough = true }, -- Deprecated suggestions
-
-    CmpItemKindCodeium = "KindConceal",
-    CmpItemKindCopilot = "KindConceal",
-    CmpItemKindDefault = "KindConceal",
-
-    -- Middle
-    CmpItemMenu = { fg = c.comment }, -- The [source of the suggestion, LSP, luasnip...]
+    CmpItemMenu = { fg = c.comment, bg = c.none }, -- The [source of the suggestion, LSP, luasnip...]
   }
-
   require("monokai-nightasty.highlights.kinds").kinds(ret, "CmpItemKind%s")
   return ret
 end
