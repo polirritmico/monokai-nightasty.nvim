a0606d55037e344c0b9c5f2aac1a67f98d0c2efa
polirritmico <ebray187@gmail.com>
2024-12-16 23:16:19 -0300

    feat(navic): Add kind highlights

--- lua/monokai-nightasty/highlights/navic.lua
+++ lua/monokai-nightasty/highlights/navic.lua
@@ -4,10 +4,12 @@ M.url = "https://github.com/SmiteshP/nvim-navic"
 
 ---@type monokai.HighlightsFn
 function M.get(c)
-  return {
+  local ret = {
     NavicText = { fg = c.fg, bg = c.none },
     NavicSeparator = { fg = c.fg, bg = c.none },
   }
+  require("monokai-nightasty.highlights.kinds").kinds(ret, "NavicIcons%s")
+  return ret
 end
 
 return M
