2073b3c1f3f334a812699617f0e2f24483202798
polirritmico <ebray187@gmail.com>
2024-12-16 23:15:37 -0300

    feat(aerial): Add kinds highlights

--- lua/monokai-nightasty/highlights/aerial.lua
+++ lua/monokai-nightasty/highlights/aerial.lua
@@ -4,11 +4,13 @@ M.url = "https://github.com/stevearc/aerial.nvim"
 
 ---@type monokai.HighlightsFn
 function M.get(c)
-  return {
+  local ret = {
     AerialNormal = { fg = c.fg, bg = c.none },
     AerialGuide = { fg = c.fg_gutter },
     AerialLine = "LspInlayHint",
   }
+  require("monokai-nightasty.highlights.kinds").kinds(ret, "Aerial%sIcon")
+  return ret
 end
 
 return M
